30e05c19e4809f62bd1f8ae8f6a3bb35d4815c87
NirBenKohal <nirbk12345@gmail.com>
2026-04-07 18:44:48 +0300

    zzz

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-#yyy
+#zzz
 def isPali(s):
     for i in range(len(s)//2): #נרוץ על חצי מחרוזת כיוון שאם החצי הראשון שווה לחצי האחרון, אז גם בדיקה מהצד השני תהיה נכונה
         if s[i] != s[-i-1]:
